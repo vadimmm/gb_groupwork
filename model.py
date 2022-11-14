5724feb97e55063a95c49041d4c8682b15946e68
Elvira <eliasufa@yahoo.com>
2022-11-15 00:23:26 +0500

    заведена папка model

---
+++ model.py
@@ -0,0 +1,18 @@
+path = 'Data/Phone.txt'
+phone_book = []
+
+def getPath():
+    global path
+    return path
+
+def setPath(new_path):
+    global path
+    path = new_path
+
+def getPhoneBook():
+    global phone_book
+    return phone_book
+
+def setPhoneBook(new_phone_book):
+    global phone_book
+    phone_book = new_phone_book
