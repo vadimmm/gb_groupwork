8a6ecbe47b62aa1cf51a714a5d1f6cb2b971b686
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-11-19 15:14:48 +0300

    GLOBAL FIX CODE v2

--- main.py
+++
@@ -1,6 +0,0 @@
-import controller
-
-def startProgram():
-    controller.start()
-
-startProgram()
--- model.py
+++
@@ -1,6 +0,0 @@
-
-import view
-def getPath():
-    global selected_path
-    return selected_path
-
